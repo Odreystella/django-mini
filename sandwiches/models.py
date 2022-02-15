2eaf9f3615753a4c9cad6ad11ebe811ca0502cfd
Oderystella <ddott59@gmail.com>
2022-02-15 17:10:20 +0900

    feat: DB 스키마 생성
    - 빵, 토핑, 치즈, 소스, 샌드위치 테이블 생성

--- sandwiches/models.py
+++ sandwiches/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Bread(models.Model):
+    name = models.CharField(max_length=32)
+    quantity = models.PositiveSmallIntegerField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=32)
+    quantity = models.PositiveSmallIntegerField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Cheese(models.Model):
+    name = models.CharField(max_length=32)
+    quantity = models.PositiveSmallIntegerField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Sauce(models.Model):
+    name = models.CharField(max_length=32)
+    quantity = models.PositiveSmallIntegerField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Sandwich(models.Model):
+    bread = models.ForeignKey(Bread, on_delete=models.CASCADE)
+    toppings = models.ManyToManyField(Topping)
+    cheese = models.ForeignKey(Cheese, on_delete=models.CASCADE)
+    sauces = models.ManyToManyField(Sauce)
+    price = models.PositiveSmallIntegerField()
